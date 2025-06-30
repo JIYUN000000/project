0e0be7c70fb80fd0fa22f013b2f35c54c0f4a054
Jiyun Kim <brilliantstar120@gmail.com>
2025-06-30 16:33:12 +0900

    commit

---
+++ ifq1.py
@@ -0,0 +1,7 @@
+N = int(input())
+s_list = list(map(int, input().split()))
+M = int(input())
+e_list = list(map(int, input().split()))
+My=input()
+print(' '.join(str(1 if k in s_list else 0) for k in e_list))
+
